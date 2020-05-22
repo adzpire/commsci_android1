ed44d325f476cf9ea59303885f30abaaa09143b9
adzpire <adz_pire@hotmail.com>
2020-05-22 16:35:04 +0700

    22/5/2020

--- app/src/main/java/com/example/commsci_android1/DashboardModern.java
+++ app/src/main/java/com/example/commsci_android1/DashboardModern.java
@@ -2,38 +2,67 @@ package com.example.commsci_android1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class DashboardModern extends AppCompatActivity implements View.OnClickListener {
+//public class DashboardModern extends AppCompatActivity implements View.OnClickListener {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_dashboard_modern);
+//
+//        CardView staffcard = (CardView) findViewById(R.id.cardstafflist);
+//
+//        staffcard.setOnClickListener(this);
+//    }
+//
+//    @Override
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.cardstafflist) {
+//            Intent intent = new Intent(this, DisplayMessageActivity2.class);
+//
+//            startActivity(intent);
+////        }else if(v.getId() == R.id.gointercom){
+////            Intent intent = new Intent(getActivity(), Intercom.class);
+////
+////            startActivity(intent);
+////        }else if(v.getId() == R.id.gomoderndash){
+////            Intent intent = new Intent(getActivity(), DashboardModern.class);
+////
+////            startActivity(intent);
+//        }
+//    }
+//}
+public class DashboardModern extends AppCompatActivity implements AdapterModerngrid.ItemClickListener {
+
+    AdapterModerngrid adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_modern);
 
-        CardView staffcard = (CardView) findViewById(R.id.cardstafflist);
+        // data to populate the RecyclerView with
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
 
-        staffcard.setOnClickListener(this);
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new AdapterModerngrid(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.cardstafflist) {
-            Intent intent = new Intent(this, DisplayMessageActivity2.class);
-
-            startActivity(intent);
-//        }else if(v.getId() == R.id.gointercom){
-//            Intent intent = new Intent(getActivity(), Intercom.class);
-//
-//            startActivity(intent);
-//        }else if(v.getId() == R.id.gomoderndash){
-//            Intent intent = new Intent(getActivity(), DashboardModern.class);
-//
-//            startActivity(intent);
-        }
+    public void onItemClick(View view, int position) {
+        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
     }
 }
---
+++ app/src/main/java/com/example/commsci_android1/DashboardModern2.java
@@ -0,0 +1,38 @@
+package com.example.commsci_android1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class DashboardModern2 extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_modern);
+
+        CardView staffcard = (CardView) findViewById(R.id.cardstafflist);
+
+        staffcard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.cardstafflist) {
+            Intent intent = new Intent(this, DisplayMessageActivity2.class);
+
+            startActivity(intent);
+//        }else if(v.getId() == R.id.gointercom){
+//            Intent intent = new Intent(getActivity(), Intercom.class);
+//
+//            startActivity(intent);
+//        }else if(v.getId() == R.id.gomoderndash){
+//            Intent intent = new Intent(getActivity(), DashboardModern.class);
+//
+//            startActivity(intent);
+        }
+    }
+}
